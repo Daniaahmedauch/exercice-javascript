8f635f9159500183ecc3006b3df0decc215bbeb0
ahmed <simoccauch16@SIMOCCAUCH16.lan>
2018-06-11 14:51:11 +0200

    boite

---
+++ boite.js
@@ -0,0 +1,33 @@
+ function a()
+ {
+   var x = 90;
+ alert (x)
+}
+a()
+/////////////////////////////
+ function a1()
+ {
+   var $alea = "nom";
+   var $aleb = "prenom";
+   var $alec = "ville";
+   alert ("nom : "+ $alea +"\n prenom"  + $aleb +"\n ville " + $alec )
+
+}
+ a1()
+
+ function el()
+ {
+ 	var $bale ="dania";
+
+ 	alert ("bonjour : "+ $bale)
+ }
+ el()
+
+ function yu()
+ {
+ 	var $kal = "nom ?";
+   var $kale = "prenom ?";
+   var $kalo = "ville ?";
+   alert ("Ton : "+ $kal +"\n Ton"  + $kale +"\n Ton " + $kalo )
+ }
+ yu()
